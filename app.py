96f5bd4eb8ebcee0971e479903d48a471aa899ad
Aleixap88 <aleixap88@gmail.com>
2017-12-17 17:21:47 +0100

    final version

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify, send_file, request, make_response, render_temp
 from flask_cors import CORS, cross_origin # habilitem cross domain api
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.utils import secure_filename
+from flask_table import create_table, Table, Col
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +11,14 @@ CORS(app)
 import sqlite3, requests, os, hashlib, uuid, json, sys
 
 auth = HTTPBasicAuth()
+
+class ItemTable(Table):
+    number = Col('Number')
+    company = Col('Company')
+    origin = Col('Origin')
+    destination = Col('Destination')
+
+
 
 @auth.get_password
 def get_password(username):
@@ -31,6 +40,7 @@ def exist_user(user):
     else:
         conn.close()
         return True
+
 
 @app.route('/menu')
 def menu():
@@ -126,6 +136,121 @@ def buscarvuelo():
         return jsonify(success=False)
     else:
         return render_template('buscarvuelo.html')
+
+@app.route('/do_buscarvuelo', methods=['POST'])
+def do_buscarvuelo():
+    vuelo_number = request.form['numero']
+    vuelo_company = request.form['companyia']
+    vuelo_origin = request.form['origen']
+    vuelo_destination = request.form['destino']
+    conn = sqlite3.connect('users.bd')
+    if (vuelo_number == "") :
+        if (vuelo_company == "") :
+            if (vuelo_origin == "") :
+                if (vuelo_destination == ""):
+                     rs = conn.execute("SELECT * FROM vuelos")
+                else:
+                     rs = conn.execute("SELECT * from vuelos where destino=?",[str(vuelo_destination)])
+            else :
+                if (vuelo_destination == ""):
+                     rs = conn.execute("SELECT * from vuelos where origen=?",[str(vuelo_origin)]);
+                else:
+                     rs = conn.execute("SELECT * from vuelos where destino=? and origen=?", [str(vuelo_destination),str(vuelo_origin)])
+        else :
+            if (vuelo_origin == "") :
+                if (destino.equals("")):
+                     rs = conn.execute("SELECT * from vuelos where companyia=?",[str(vuelo_origin)])
+                else:
+                     rs = conn.execute("SELECT * from vuelos where companyia=? and destino=?",[str(vuelo_company),str(vuelo_destination)])
+            else :
+                if (vuelo_destination == ""):
+                     rs = conn.execute("SELECT * from vuelos where companyia=? and origen=?",[str(vuelo_company),str(vuelo_origin)])
+                else:
+                     rs = conn.execute("SELECT * from vuelos where companyia=? and destino=? and origen=?",[str(vuelo_company),str(vuelo_destination),str(vuelo_origin)])
+    else :
+        if (vuelo_company == ""):
+            if (vuelo_origin == ""):
+                if (vuelo_destination == ""):
+                    rs = conn.execute("SELECT * from vuelos where id_vuelo=?",[str(vuelo_number)])
+                else:
+                    rs = conn.execute("SELECT * from vuelos where id_vuelo=? and destino=?",[str(vuelo_number),str(vuelo_destination)])
+            else :
+                if (vuelo_destination == ""):
+                    rs = conn.execute("SELECT * from vuelos where id_vuelo=? and origen=?",[str(vuelo_number), str(vuelo_origin)])
+                else:
+                    rs = conn.execute("SELECT * from vuelos where id_vuelo=? and destino=? and origen=?", [str(vuelo_number),str(vuelo_destination),str(vuelo_origin)])
+        else :
+            if (vuelo_origin == "") :
+                if (vuelo_destination == ""):
+                    rs = conn.execute("SELECT * from vuelos where id_vuelo=? and companyia=?", [str(vuelo_number), str(vuelo_company)])
+                else:
+                    rs = conn.execute("SELECT * from vuelos where id_vuelo=? and companyia=? and destino=?",[str(vuelo_number),str(vuelo_company),str(vuelo_destination)])
+            else :
+                if (vuelo_destination == ""):
+                    rs = conn.execute("SELECT * from vuelos where id_vuelo=? and companyia=? and origen=?", [str(vuelo_number),str(vuelo_company),str(vuelo_origin)])
+                else:
+                    rs = conn.execute("SELECT * from vuelos where id_vuelo=? and companyia=? and destino=? and origen=?",[str(vuelo_number),str(vuelo_company),str(vuelo_destination),str(vuelo_origin)])
+
+    return """
+        <!DOCTYPE html>
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head> 
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <title>Menu</title>
+            <link rel="stylesheet" type="text/css" href="/static/menu.css" />
+            <link rel="stylesheet" tupe="text/css" href="/static/tabla.css" />
+        </head>
+        <body>
+            <ul>
+                <li><a href="menu">Home</a></li>
+                <li class="dropdown">
+                    <a href="#">Flights</a>
+                    <div class="dropdown-content">
+                        <a href="altavuelo">Register Flight</a>
+                        <a href="buscarvuelo">Search Flight</a>
+                    </div> 
+                </li>
+                
+               <li class="dropdown">
+                    <a href="#">Hotels</a>
+                    <div class="dropdown-content">
+                        <a href="altahotel">Register Hotel</a>
+                        <a href="buscarhotel">Search Hotel</a>
+                    </div> 
+                </li>
+                <li><a href="logout">Log Out</a></li>
+            </ul>
+            <div class="container">
+                <div class="login-box">
+                    <div class="box-header">
+                        <h2>Searched Flights</h2>
+                    </div>
+                <table id="customers">
+            <tr>
+                <th><strong>Flight Number</strong></th>
+                <th><strong>Company</strong></th>
+                <th><strong>Origin</strong></th>
+                <th><strong>Destination</strong></th>
+            </tr>
+                <tr>
+                    <td>123</td>
+                    <td>Airlines</td>
+                    <td>Madrid</td>
+                    <td>Barcelona</td>
+                </tr>
+                <tr>
+                    <td>1235</td>
+                    <td>Vueling</td>
+                    <td>Granada</td>
+                    <td>Menorca</td>
+                </tr>
+        
+        </body>
+    </html>"""
+    conn.commit()
+    conn.close()
+
+
 
 @app.route('/altahotel')
 def altahotel():
